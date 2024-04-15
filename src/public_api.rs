e407f919d913a9dbb4b3dfbe1f40b9669bd9ba1e
Morgan Adamiec <morgan@parity.io>
2024-04-15 08:27:26 +0100

    Download crates first

--- src/public_api.rs
+++ src/public_api.rs
@@ -1,17 +1,19 @@
 use anyhow::Result;
 use cargo::{
-    core::Workspace,
-    sources::{source::Source, RegistrySource},
+    core::{PackageSet, Workspace},
+    sources::{source::SourceMap, RegistrySource},
     util::cache_lock::CacheLockMode,
     util_semver::VersionExt,
 };
-use std::{collections::HashSet, io::Write};
+use std::collections::HashSet;
+use std::io::Write;
 use termcolor::{ColorChoice, StandardStream};
 
 use crate::{cli::Semver, registry};
 
 pub fn handle_public_api(breaking: Semver) -> Result<()> {
     let mut stdout = StandardStream::stdout(ColorChoice::Auto);
+    let mut stderr = StandardStream::stderr(ColorChoice::Auto);
     let config = cargo::Config::default()?;
     config.shell().set_verbosity(cargo::core::Verbosity::Quiet);
     let path = breaking.path.canonicalize()?.join("Cargo.toml");
@@ -21,23 +23,19 @@ pub fn handle_public_api(breaking: Semver) -> Result<()> {
         .config()
         .acquire_package_cache_lock(CacheLockMode::DownloadExclusive)?;
     let mut reg = registry::get_registry(&workspace)?;
+    let mut upstreams = Vec::new();
 
-    writeln!(stdout, "looking up crates...",)?;
+    writeln!(stderr, "looking up crates...",)?;
     registry::download_crates(&mut reg, &workspace, false)?;
-    drop(_lock);
 
-    writeln!(stdout, "compiling crates...",)?;
+    writeln!(stderr, "downloading crates...",)?;
 
     for c in workspace.members() {
         if c.publish().is_some() {
             continue;
         }
 
-        let _lock = workspace
-            .config()
-            .acquire_package_cache_lock(CacheLockMode::DownloadExclusive)?;
         let upstream = registry::get_crate(&mut reg, c.name())?;
-        drop(_lock);
         let upstream = upstream
             .iter()
             .filter(|c| !c.is_yanked())
@@ -45,6 +43,43 @@ pub fn handle_public_api(breaking: Semver) -> Result<()> {
             .max_by_key(|c| c.as_summary().version());
 
         let Some(upstream) = upstream else {
+            continue;
+        };
+
+        upstreams.push(upstream.clone());
+    }
+
+    let ids = upstreams.iter().map(|c| c.package_id()).collect::<Vec<_>>();
+    let mut sources = SourceMap::new();
+    for c in &upstreams {
+        let c = Box::new(RegistrySource::remote(
+            c.as_summary().source_id(),
+            &HashSet::new(),
+            workspace.config(),
+        )?);
+        sources.insert(c);
+    }
+    let download = PackageSet::new(&ids, sources, workspace.config())?;
+    let mut downloads = download.enable_download()?;
+    let mut upstreams = Vec::new();
+
+    for id in download.package_ids() {
+        if let Some(pkg) = downloads.start(id)? {
+            upstreams.push(pkg.clone());
+        }
+    }
+
+    while downloads.remaining() != 0 {
+        upstreams.push(downloads.wait()?.clone());
+    }
+
+    drop(downloads);
+    drop(download);
+    drop(_lock);
+    writeln!(stderr, "building crates...",)?;
+
+    for c in workspace.members() {
+        let Some(upstream) = upstreams.iter().find(|u| c.name() == u.name()) else {
             continue;
         };
 
@@ -56,13 +91,6 @@ pub fn handle_public_api(breaking: Semver) -> Result<()> {
             .build()?;
 
         let new = public_api::Builder::from_rustdoc_json(json_path).build()?;
-
-        let upstream = Box::new(RegistrySource::remote(
-            upstream.as_summary().source_id(),
-            &HashSet::new(),
-            workspace.config(),
-        )?)
-        .download_now(upstream.as_summary().package_id(), workspace.config())?;
 
         let json_path = rustdoc_json::Builder::default()
             .toolchain("nightly")
